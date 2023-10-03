fc639d56a8683e56c2bfb1d15683e30a3b62c84a
Kishlay <kishlaykumarkashyap@gmail.com>
2023-10-03 11:21:18 +0530

    added log file

--- HTTP Server/index.js
+++ HTTP Server/index.js
@@ -1,8 +1,12 @@
 const http = require('http');
-
+const fs = require("fs");
 const server = http.createServer((req, res) => {
+  const log = `${ Date.now()} ${req.url} New Req Received\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
   //res.end() used to send a response to an HTTP request. It indicates the end of the response stream and sends any optional data as the response body.  
-  res.end('Hello, Node.js HTTP Server!');
+    res.end('Hello, Node.js HTTP Server!');
+  });
+  
 });
 
 const port = 3000;
